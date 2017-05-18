2ba9f836959d0e8be6c49951a6fcc95cbfdfebf8
nzho8446@uni.sydney.edu.au <nzho8446@uni.sydney.edu.au>
2017-05-19 01:09:44 +1000

    good banter state

---
+++ Eclipse_WorkSpace/Assignment 3/src/src/usyd/it/olympics/data/BasicEntity.java
@@ -0,0 +1,11 @@
+package src.usyd.it.olympics.data;
+
+import java.util.HashMap;
+
+/**
+ * Created by INFO2120
+ */
+public interface BasicEntity {
+
+    public HashMap<String, Object> asHashMap();
+}
